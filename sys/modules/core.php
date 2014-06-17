434a9d7a39ab35591befebde8e2b447586afd915
acid-cedric <cedric@acid.fr>
2014-06-17 11:53:19 +0200

    coquilles

--- sys/modules/core.php
+++ sys/modules/core.php
@@ -15,11 +15,11 @@
  */
 
 /**
- * Permet à l'utilisateur d'altérer tous les AcidModule 
+ * Permet à l'utilisateur d'altérer tous les AcidModule
  * @package   User Module
  */
-abstract class AcidModule extends AcidModuleCore { 
-	
+abstract class AcidModule extends AcidModuleCore {
+
 	/**
 	 * Retourne l'url du module
 	 * @param array $vals
@@ -28,13 +28,37 @@ abstract class AcidModule extends AcidModuleCore {
 	public static function buildUrl($vals=array()) {
 		return Route::buildUrl(static::checkTbl(),$vals);
 	}
-	
+
 	/**
-	 * Retourn l'url associé à l'objet
+	 * Retourne l'url associé à l'objet
 	 * @return string
 	 */
 	public function url() {
 		return $this->buildUrl($this->getVals());
 	}
-	
+
+	/**
+	 * Methode d'affichage des champs multilangues
+	 * @param array $elt
+	 * @param string $key
+	 * @param int $split
+	 * @return string
+	 */
+	public static function printAdminListLang($elt,$key,$split=null) {
+		$val = '';
+		$m = static::build($elt);
+
+		$val = '<table style="width:100%; min-width:0px;">';
+		foreach (Acid::get('lang:available') as $l) {
+			$value = $m->get($m->langKey($key,$l));
+			$value = $split ? AcidVarString::split($value,$split) : $value;
+			if ($value) {
+				$val .= '<tr><td style="width:15px;" ><img style="max-width:15px;" src="'. Acid::get('url:img') . 'langs/'.$l.'_sel.png" alt="'.$l.'" title="'.$l.'" /><td>'.$value.'</td></td></tr>';
+			}
+		}
+		$val .= '</table>';
+
+		return $val;
+	}
+
 }
