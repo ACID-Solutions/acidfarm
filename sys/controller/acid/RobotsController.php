8361f829d934c75131e9479b71e03f8c4b34c200
acid-cedric <cedric@acid.fr>
2015-02-05 19:01:22 +0100

    robots.txt

--- sys/controller/acid/RobotsController.php
+++ sys/controller/acid/RobotsController.php
@@ -20,21 +20,50 @@
  * @package   Controller
  */
 class RobotsController{
-	
+
+	public static function getThemes() {
+
+		$themes = array();
+		$path = SITE_PATH.Acid::get('keys:theme');
+
+		if (is_dir($path)) {
+			if ($handle = opendir($path)) {
+				while (false !== ($entry = readdir($handle))) {
+					if (!in_array($entry,array('.','..'))) {
+						if (is_dir($path.'/'.$entry)) {
+							$themes[$entry] = $entry;
+						}
+					}
+				}
+				closedir($handle);
+			}
+
+		}
+
+		return $themes;
+	}
+
 	/**
 	 * Fichier robots.txt
 	 */
     public function index(){
       	Acid::set('out','text');
 
+      	$disallow = '';
+      	$themes = self::getThemes();
+      	foreach ($themes as $theme) {
+      		$disallow .= 'Disallow: '.Acid::get('url:folder') . Acid::get('keys:theme') . '/'.$theme.'/css/'  . "\n" ;
+			$disallow .= 'Disallow: '.Acid::get('url:folder') . Acid::get('keys:theme') . '/'.$theme.'/out/'  . "\n" ;
+			$disallow .= 'Disallow: '.Acid::get('url:folder') . Acid::get('keys:theme') . '/'.$theme.'/tpl/'  . "\n" ;
+			$disallow .= 'Disallow: '.Acid::get('url:folder') . Acid::get('keys:theme') . '/'.$theme.'/js/'  . "\n" ;
+      	}
+
 		$robot = 	'User-agent: *' . "\n" .
-				'' . "\n" .
-				'Disallow: '.Acid::get('url:css')  . "\n" .
-				'Disallow: '.Acid::get('url:out')  . "\n" .
-				'Disallow: '.Acid::get('url:tpl')  . "\n" .
-				'Disallow: '.Acid::get('url:js')  . "\n" .
-				'' . "\n";
-		
+					$disallow .
+					'' . "\n";
+
+
+
 		Conf::addToContent($robot);
     }
 }
