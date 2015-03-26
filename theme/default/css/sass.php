664744e36815ba3770cc3e0e3b5a3dab37a2d933
acid-cedric <cedric@acid.fr>
2015-03-26 18:49:45 +0100

    sass environement

--- theme/default/css/sass.php
+++ theme/default/css/sass.php
@@ -3,5 +3,10 @@
 $directory = "stylesheets";
 require '../../../sys/glue.php';
 
+$sass_environement = __DIR__.'/sass/_environement.scss';
+$sass = '$sitepath:"'.Acid::get('url:folder') .'";'. "\n" .
+		'$basepath:"'.Acid::get('url:theme') .'";'. "\n" ;
+file_put_contents($sass_environement,$sass);
+
 Acid::load(Acid::get('externals:sass:path:lib'));
 scss_server::serveFrom(__DIR__);
