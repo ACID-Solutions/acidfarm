61a3d8e64ffe09a460f44ff338c794333332952e
Cédric <cedric@acid.fr>
2015-09-09 19:05:03 +0200

    access level bootstrap menu

--- theme/bootstrap/out/functions.php
+++ theme/bootstrap/out/functions.php
@@ -24,14 +24,19 @@ class Func extends FuncCore {
             $menu_elts = array();
 
             foreach ($config['siteadmin_cat'] as $key => $cat) {
-                $menu_elts[$key] = array('url'=>AcidUrl::build(array('page'=>$key)),'name'=>Lib::getIn('label',$cat),'unclickable'=>Lib::getIn('unclickable',$cat));
-                if ($elts = Lib::getIn('elts',$cat)) {
-                    foreach ($elts as $skk => $sk)
-                    {
-                        $selt = Acid::get('admin:menu_config:controller:'.$sk);
-                        $menu_elts[$key]['elts'][$sk] = array('url'=>AcidUrl::build(array('page'=>$sk)),'name'=>Lib::getIn('label',$selt));
-                    }
+                if (User::curLevel(Lib::getIn('level',$cat,0))) {
+                    $menu_elts[$key] = array('url'=>AcidUrl::build(array('page'=>$key)),'name'=>Lib::getIn('label',$cat),'unclickable'=>Lib::getIn('unclickable',$cat));
+                    if ($elts = Lib::getIn('elts',$cat)) {
+                        foreach ($elts as $skk => $sk)
+                        {
 
+                            $selt = Acid::get('admin:menu_config:controller:' . $sk);
+                            if (User::curLevel(Lib::getIn('level',$selt,0))) {
+                                $menu_elts[$key]['elts'][$sk] = array('url' => AcidUrl::build(array('page' => $sk)), 'name' => Lib::getIn('label', $selt));
+                            }
+                        }
+
+                    }
                 }
             }
 
