51a79f4d37099a2b5ba7a0a9ece8ea84b72a58b8
Cédric <cedric@acid.fr>
2015-11-12 17:19:27 +0100

    versionning apache ext

--- acid/tools/template.php
+++ acid/tools/template.php
@@ -153,11 +153,17 @@ class AcidTemplate {
 			if ($version = self::versioningVal()) {
 				if (strpos($url,'http')!==0) {
 					$base_extension = AcidFs::getExtension($url);
-					$base_url = AcidFs::removeExtension($url);
-					$base_tag = Acid::get('versioning:tag') ? Acid::get('versioning:way') : '-__VERSION__';
-					$base_url_new = $base_url.str_replace('__VERSION__',$version,$base_tag).'.'.$base_extension;
+					$available_ext =  Acid::get('versioning:ext') ? Acid::get('versioning:ext') : array('css','js');
 
-					return $base_url_new;
+					if (in_array($base_extension,$available_ext)) {
+						$base_url = AcidFs::removeExtension($url);
+						$base_tag = Acid::get('versioning:tag') ? Acid::get('versioning:way') : '-__VERSION__';
+						$base_url_new = $base_url.str_replace('__VERSION__',$version,$base_tag).'.'.$base_extension;
+
+						return $base_url_new;
+					}
+
+					return $url;
 				}
 			}
 		}
