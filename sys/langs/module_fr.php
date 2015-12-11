d8f7aef45b7736bc439e1ed1f2af6c710bf04050
Cédric <cedric@acid.fr>
2015-12-11 12:39:38 +0100

    pre config sass + module_fr.php sample

---
+++ sys/langs/module_fr.php
@@ -0,0 +1,21 @@
+<?php
+
+//--config
+//$lang['mod']['config']['__NAME__'] 	 = 'Configuration';
+//$lang['mod']['config']['id_config']  = 'Id';
+//$lang['mod']['config']['name']       = 'Nom';
+//$lang['mod']['config']['value']      = 'Valeur';
+//
+//$lang['mod']['config']['email']      = 'Email';
+//$lang['mod']['config']['contact']    = 'Contact';
+//
+//$lang['mod']['config']['address']    = 'Adresse';
+//$lang['mod']['config']['cp']     	 = 'Code Postal';
+//$lang['mod']['config']['city']  	 = 'Ville';
+//
+//$lang['mod']['config']['coords']  	 = 'Lat,Long';
+//$lang['mod']['config']['zoom']  	 = 'Zoom';
+//
+//$lang['mod']['config']['phone'] 	 = 'Téléphone';
+//$lang['mod']['config']['fax']        = 'Fax';
+//$lang['mod']['config']['website']    = 'Site Web';
