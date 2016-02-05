85c954f63a95f6c7ccbb40e5f9b71787ac9d3809
Cédric <cedric@acid.fr>
2016-02-05 19:29:36 +0100

    correctif pour php7

---
+++ registration/post.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cedric
+ * Date: 07/09/15
+ * Time: 12:32
+ */
+
+$acid_page_type = 'rest';
+
+
+include pathinfo(__FILE__,PATHINFO_DIRNAME ).'/../sys/glue.php';
+
+
+include('private/registration.php');
