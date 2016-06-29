91841a9bc9e614a8cc7caf75396a12b6c812e107
Cédric <cedric@acid.fr>
2016-06-29 17:59:37 +0200

    body.tpl

--- theme/bootstrap/out/default.php
+++ theme/bootstrap/out/default.php
@@ -25,22 +25,20 @@ if ($page_class&&$nav_class) {
 	$body = $this->getBodyAttrs();
 	$body_class = isset($body['class']) ? $body['class'] : '';
 	$body_class = trim($body_class.' '.$page_class.' '.$nav_class.' '.$site_class);
-	if ($body_class) {
-		$this->setBodyAttrs(array('class'=>$body_class));
-	}
 }
 
-$bhead='header';
-$bfoot = 'footer';
 if (isset($_SERVER['HTTP_USER_AGENT'])) {
 	$nav_old = false;
 	foreach (array('MSIE 8','MSIE 7','MSIE 6') as $search) {
 		$nav_old = $nav_old || (stripos($_SERVER['HTTP_USER_AGENT'],$search) !== false);
 	}
 	if ($nav_old) {
-		$bhead='div';
-		$bfoot = 'div';
+		$body_class .= ' nav-old';
 	}
+}
+
+if ($body_class) {
+	$this->setBodyAttrs(array('class'=>$body_class));
 }
 
 //RESPONSIVE MOBILE
