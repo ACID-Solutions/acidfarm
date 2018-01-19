53bbe796371cac9f211e271af02e7d6e1dec2f4d
Cédric <cedric@acid.fr>
2018-01-19 15:39:26 +0100

    sentry ignore errors sample

--- sys/supervisor.php
+++ sys/supervisor.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * AcidFarm - Yet Another Framework
- *
  * Requires PHP version 5.3
  *
  * @author    ACID-Solutions <contact@acid-solutions.fr>
@@ -15,30 +14,37 @@
  */
 
 if (Acid::get('sentry:url')) {
-
     try {
-            $client = new Raven_Client(Acid::get('sentry:url'));
-
-            $sentry_report_level = ( (Acid::exists('sentry:report_level') ? Acid::get('sentry:report_level') : -1 ) );
-
-            $error_handler = new Raven_ErrorHandler($client);
-            $error_handler->registerExceptionHandler();
-            $error_handler->registerErrorHandler(true, $sentry_report_level);
-            $error_handler->registerShutdownFunction();
-
-            if (Acid::get('session:enable')) {
-                $client->user_context(User::curUser()->getVals());
-                $client->extra_context(array('php_version' => phpversion(), 'session' => AcidSession::getInstance()->data));
-            }else{
-                $client->user_context(array('session disabled'));
-                $client->extra_context(array('php_version' => phpversion(), 'session' => array()));
+        $client = new Raven_Client(Acid::get('sentry:url'));
+        
+        /*$client->setSendCallback(function ($data) {
+            $error =
+                !empty($data['exception']['values'][0]['value']) ? $data['exception']['values'][0]['value'] : false;
+            if ($error) {
+                if (strpos($error, 'should be compatible') !== false) {
+                    return false;
+                }
             }
-
+        });*/
+        
+        $sentry_report_level = ((Acid::exists('sentry:report_level') ? Acid::get('sentry:report_level') : -1));
+        
+        $error_handler = new Raven_ErrorHandler($client);
+        $error_handler->registerExceptionHandler();
+        $error_handler->registerErrorHandler(true, $sentry_report_level);
+        $error_handler->registerShutdownFunction();
+        
+        if (Acid::get('session:enable')) {
+            $client->user_context(User::curUser()->getVals());
+            $client->extra_context(['php_version' => phpversion(), 'session' => AcidSession::getInstance()->data]);
+        } else {
+            $client->user_context(['session disabled']);
+            $client->extra_context(['php_version' => phpversion(), 'session' => []]);
+        }
     } catch (Exception $e) {
         trigger_error(
-                'Unable to work with Raven_Client due to sentry url :'.Acid::get('sentry:url'). "\n" .
-                "Exception reçue : ".$e->getMessage()."\n"
+            'Unable to work with Raven_Client due to sentry url :' . Acid::get('sentry:url') . "\n" .
+            "Exception reçue : " . $e->getMessage() . "\n"
         );
     }
-
 }
